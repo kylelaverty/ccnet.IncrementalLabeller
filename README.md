f65346a03934d7f87a0221bd2a4f6652e89089c0
Kyle Laverty <klaverty@miovision.com>
2018-09-11 14:09:24 -0400

    Initial commit of the labeller

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# ccnet.IncrementalLabeller
-CCNet labeller plugin: Builds a label based on the last label and increments it by the provided value.
+# Incremental labeller plugin for CCNET
+
+This labeller plugin builds the CCNet internal label from the last label combined with a specified increment.
+
+As any other CCNet labeller, used in conjunction with the AssemblyInfo task in  [MsBuildTasks](https://github.com/loresoft/msbuildtasks) or with a manual script it allows setting the AssemblyVersion - AssemblyFileVersion attributes of an MSBuild project.
+
+## Building the label
+
+* Take the last label
+* Take the Major, Minor and Build if configured and overwrite the last label
+* If an increment is specified, increment the revision by that, otherwise increment the revision by 1
+* If the Major/Minor/Build value has changed since the last label, and reset revision is set, update revision to 0
+* If the last run is not success and increment of failure is false, reset revision to the last label value
+* New label: [Major].[Minor].[Build].[Revision]
+
+## Usage
+```xml
+	<labeller type="incrementalLabeller">
+		<major>10</major>
+		<minor>20</minor>
+		<build>30</build>
+		<revisionIncrement>10</revisionIncrement>
+		<resetRevisionAfterVersionChange>true</resetRevisionAfterVersionChange>
+		<incrementOnFailure>false</incrementOnFailure>
+	</labeller>
+```
+
+* *major* is optional (defaults to previous value)
+* *minor* is optional (defaults to previous value)
+* *build* is optional  (defaults to previous value)
+* *revisionIncrement* is optional (defaults to 1)
+* *resetRevisionAfterVersionChange* is optional (defaults to false)
+* *incrementOnFailure* is optional (defaults to false)
+
+## How to make use of the label	
+
+* In an MSBuild target you may access the value as $(CCNetLabel)
+* In NAnt you may use $[CCNetLabel]
+* To use the value inside the CCNet config blocks, you need to write it as a dynamic parameter, i.e. $[$CCNetLabel] . This is valid only for CCNet 1.5+
+
+## Installation ##
+
+* Before building, update the *ThoughtWorks.CruiseControl.Core.dll* and *ThoughtWorks.CruiseControl.Remote.dll* in the *lib* folder with the ones corresponding to your CCNET version (found in *server* subfolder of the CruiseControl.Net program files folder)
+
+* Build the solution
+
+* Copy the resulting *ccnet.IncrementalLabeller.plugin.dll* into the *server* subfolder.
+
+NOTE: the assembly file name must follow the ccnet.*.plugin.dll pattern - in order to be loaded by CCNET
---
+++ ccnet.IncrementalLabeller.plugin/IncrementalLabeller.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using ThoughtWorks.CruiseControl.Core.Label;
+
+namespace CcNet.Labeller {
+    [ReflectorType("incrementalLabeller")]
+    public class IncrementalLabeller : LabellerBase, ILabeller {
+
+        /// <summary>
+        /// Major number component of the version. 
+        /// </summary>
+        /// <version>1.4.4</version>
+        /// <default>0</default>
+        [ReflectorProperty("major", Required = false)]
+        public int Major { get; set; }
+
+        /// <summary>
+        /// Minor number component of the version. 
+        /// </summary>
+        /// <version>1.4.4</version>
+        /// <default>0</default>
+        [ReflectorProperty("minor", Required = false)]
+        public int Minor { get; set; }
+
+        /// <summary>
+        /// Build number component of the version. If not specified the build number is incremented on every successful integration. 
+        /// </summary>
+        /// <version>1.4.4</version>
+        /// <default>0</default>
+        [ReflectorProperty("build", Required = false)]
+        public int Build { get; set; }
+
+        /// <summary>
+        /// Specifies an optional revision increment value, if omitted, the revision will increment by one
+        /// </summary>
+        /// <default>0</default>
+        [ReflectorProperty("revisionIncrement", Required = false)]
+        public int RevisionIncrement { get; set; }
+
+        /// <summary>
+        /// Whether to increase the build number component if the integration fails. By default the build number component will only increase
+        /// if the integration was successful.
+        /// </summary>
+        /// <version>1.4.4</version>
+        /// <default>false</default>
+        [ReflectorProperty("incrementOnFailure", Required = false)]
+        public bool IncrementOnFailure { get; set; }
+
+        /// <summary>
+		/// Gets or sets a value indicating whether to reset revision number after a version change.
+		/// </summary>
+		/// <value>
+		/// 	<c>true</c> if the revision number should be reset after a version change; otherwise, <c>false</c>.
+		/// </value>
+        /// <default>false</default>
+		[ReflectorProperty("resetRevisionAfterVersionChange", Required = false)]
+		public bool ResetRevisionAfterVersionChange { get; set; }
+
+        public override string Generate(IIntegrationResult integrationResult) {
+
+            Version oldVersion;
+
+            // Try getting old version.
+            try {
+                Log.Debug(string.Concat("[incrementalLabeller] Old build label is: ", integrationResult.LastIntegration.Label));
+                oldVersion = new Version(integrationResult.LastIntegration.Label);
+            } catch (Exception) {
+                oldVersion = new Version(Major, Minor, Build, 0);
+            }
+
+            Log.Debug(string.Concat("[incrementalLabeller] Old version is: ", oldVersion.ToString()));
+
+            int currentMajor = (Major > 0) ? Major : oldVersion.Major;
+            int currentMinor = (Minor > 0) ? Minor : oldVersion.Minor;
+            int currentBuild = (Build > 0) ? Build : oldVersion.Build;
+            int currentRevision = (RevisionIncrement > 0) ? oldVersion.Revision + RevisionIncrement : oldVersion.Revision + 1;
+
+            // If the last build failed and increment on failure is false, reset current revision to old revision.
+            if (integrationResult.LastIntegrationStatus != IntegrationStatus.Success && !IncrementOnFailure) {
+                Log.Debug("[incrementalLabeller] Last build in failure state, revision will not be incremented");
+                currentRevision = oldVersion.Revision;
+            }
+
+            // If the version has changed, figure out what to do to the revision number.
+            if(currentMajor != oldVersion.Major || currentMinor != oldVersion.Minor || currentBuild != oldVersion.Build){
+                Log.Debug("[incrementalLabeller] Version change detected");
+
+                if(ResetRevisionAfterVersionChange){
+                    currentRevision = 0;
+                }
+            }
+
+            var newVersion = new Version(currentMajor,currentMinor, currentBuild, currentRevision);
+
+            Log.Debug("[incrementalLabeller] New Version is: {0}", newVersion);
+            
+            return newVersion.ToString();
+        }
+    }
+}
